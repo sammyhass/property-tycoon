26e742d479007716aabe9061d74bbf7432563056
Sammy Hass <sammyhass01@gmail.com>
2022-01-27 19:25:53 +0000

    fix: types

--- src/scripts/gen-db-types.js
+++ src/scripts/gen-db-types.js
@@ -1,15 +1,15 @@
 const { exec } = require('child_process');
 const dotenv = require('dotenv');
 const { assert } = require('console');
-
+/*
+ * Generate types from supabase schema, and write to src/scripts/db-types.ts
+ */
 dotenv.config();
 
 exec(
   `npx openapi-typescript "${process.env.SUPABASE_URL}/rest/v1/?apikey=${process.env.SUPABASE_ANON_KEY}" --output src/types/db-types.ts`,
   () => {},
   (error, stdout, stderr) => {
-    if (error) {
-      console.warn(`exec error: ${error}`);
-    }
+    assert(!error, `exec error: ${error}`);
   }
 );
