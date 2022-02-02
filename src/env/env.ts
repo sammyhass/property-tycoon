2449c5fa0066a64ceda9c2ef58654f3e239edbdf
Sammy Hass <sammyhass01@gmail.com>
2022-02-02 02:14:13 +0000

    fix env var on prod

--- src/env/env.ts
+++ src/env/env.ts
@@ -1,1 +1,4 @@
-export const API_URL = 'http://localhost:3000/api';
+export const API_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://property-tycoon.vercel.app/api'
+    : 'http://localhost:3000/api';
