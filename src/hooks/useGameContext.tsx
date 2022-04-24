481da45ad85f8fae9a67dae03a874b39094654b0
Sammy Hass <sammyhass01@gmail.com>
2022-04-24 01:48:14 +0100

    fix:

--- src/hooks/useGameContext.tsx
+++ src/hooks/useGameContext.tsx
@@ -1506,6 +1506,7 @@ export const GameContextProvider = ({
         gameSettings,
         showActionModal,
         canEndTurn,
+        mode: gameMode,
         landedOnFreeParking,
         payToFreeParking,
         buy,
